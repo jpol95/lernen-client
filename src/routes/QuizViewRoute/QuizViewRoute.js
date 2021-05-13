38db78d9cd4ac9386260feba1d2f94e79d6bb474
Jesse Pollack <jesse.lincolndouglas@gmail.com>
2021-05-13 12:35:54 -0400

    making the quiz view

--- src/routes/QuizViewRoute/QuizViewRoute.js
+++ src/routes/QuizViewRoute/QuizViewRoute.js
@@ -1,15 +1,31 @@
 import React, { useEffect, useState } from "react";
 import QuestionApiService from "../../services/question-api-service";
+import "./QuizView.css";
 
 export default function QuizViewRoute(props) {
-    const [questions, setQuestions] = useState([])
-    useEffect(() => {
-        const loadData = async () => {
-            setQuestions(await QuestionApiService.getQuizQuestions(props.match.params.id))
-        }
-        loadData()
-    }, [])
-    return <ul className="quiz-view">
-        {questions.map(question => <li className="question-view">{question.title}</li>)}
+  const [questions, setQuestions] = useState([]);
+  useEffect(() => {
+    const loadData = async () => {
+      setQuestions(
+        await QuestionApiService.getQuizQuestions(props.match.params.id)
+      );
+    };
+    loadData();
+  }, []);
+  console.log(questions);
+  return (
+    <ul className="quiz-view">
+      {questions.map((question, index) => (
+        <li key={question.id} className="question-view">
+          {index + 1}. {question.title}
+          <br />
+          {question.answers.split(",").map((answer) => (
+            <>
+              {answer} <br />{" "}
+            </>
+          ))}
+        </li>
+      ))}
     </ul>
+  );
 }

a6522b65e0f8e9f66803a17b3933f67398ae67ef
Jesse Pollack <jesse.lincolndouglas@gmail.com>
2021-05-13 14:08:35 -0400

    got the correct and incorrect answers to register

--- src/routes/QuizViewRoute/QuizViewRoute.js
+++ src/routes/QuizViewRoute/QuizViewRoute.js
@@ -4,28 +4,46 @@ import "./QuizView.css";
 
 export default function QuizViewRoute(props) {
   const [questions, setQuestions] = useState([]);
+  const [selected, setSelected] = useState([])
   useEffect(() => {
     const loadData = async () => {
+      const loadedQs = await QuestionApiService.getQuizQuestions(props.match.params.id)
       setQuestions(
-        await QuestionApiService.getQuizQuestions(props.match.params.id)
+        loadedQs
       );
+      setSelected(new Array(loadedQs.length).fill({}))
     };
     loadData();
   }, []);
-  console.log(questions);
+
+  console.log(selected)
+  function onAnswer(e, index){
+      e.persist()
+    setSelected(prevSelected => prevSelected.map((current, sIndex) => {
+        console.log(e)
+        if (index !== sIndex) return current
+        else return {correct: Number(e.target.value.split(",")[0]), attempted: Number(e.target.value.split(",")[1])}
+    }))
+  }
   return (
+   <>
     <ul className="quiz-view">
       {questions.map((question, index) => (
         <li key={question.id} className="question-view">
           {index + 1}. {question.title}
           <br />
-          {question.answers.split(",").map((answer) => (
+          <form>
+          {question.answers.split(",").map((answer, aIndex) => (
             <>
-              {answer} <br />{" "}
+                <input onChange={(e) => onAnswer(e, index)} key={`answer${index}${aIndex}`} name={`question${index}`} type="radio" id={`question${index}answer${aIndex}`} value={`${question.correct_answer},${aIndex}`} />
+                <label key={`label${index}${aIndex}`} htmlFor={index}> {answer}</label><br />
             </>
           ))}
+          </form>
         </li>
       ))}
     </ul>
+    <button className="submit" type="submit" > Submit Quiz </button>
+</>
   );
 }
